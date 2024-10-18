88d2110144f336ed42105d8c6757ef595d771409
Nikhil-Porwal <nik.porwal9340@gmail.com>
2024-10-18 12:54:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -47,10 +47,10 @@ function rollDice(n) {
 rollDice(1);
 rollDice(2);
 if (randomNumber1>randomNumber2) {
-    document.querySelector("h1").textContent = "🚩Player 1 Wins!";
+    document.querySelector("h1").textContent = "Player 1 Wins!";
 }
 else if(randomNumber1<randomNumber2){
-    document.querySelector("h1").textContent = "Player 2 Wins!🚩";
+    document.querySelector("h1").textContent = "Player 2 Wins!";
 }
 else{
     document.querySelector("h1").textContent = "Draw!";
